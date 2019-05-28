5b52d55d1fc8fe186d3c7f06c55668b454c21e41
SC <SC@StrietzelCosMBP.home>
2019-05-28 15:08:58 -0700

    Messing around and added funfun

---
+++ funfun.py
@@ -0,0 +1,13 @@
+""" Testing if this actually works
+"""
+
+class Dog:
+
+     def __init__(self, name):
+         self.name = name
+         self.tricks = [] 
+
+     def add_trick(self, trick):
+         self.tricks.append(trick)
+
+
--- lambdata_tbradshaw91/example_module.py
+++ lambdata_tbradshaw91/example_module.py
@@ -2,3 +2,11 @@ x = 2
 
 def increment(number):
   return number + 1
+
+def happyBirthday(person):
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear " + person + ".")
+    print("Happy Birthday to you!")
+
+
