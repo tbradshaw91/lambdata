1d57d1dcf7e1830b0bfb6b8e35a59dc64f958199
SC <SC@StrietzelCosMBP.home>
2019-05-28 15:48:01 -0700

    Changes to be committed: 	modified:   funfun.py

--- funfun.py
+++ funfun.py
@@ -2,12 +2,19 @@
 """
 
 class Dog:
+  def __init__(self, name):
+      self.name = name
+      self.tricks = []
 
-     def __init__(self, name):
-         self.name = name
-         self.tricks = [] 
+  def add_trick(self, trick):
+      self.tricks.append(trick)
 
-     def add_trick(self, trick):
-         self.tricks.append(trick)
+class DataCleaner:
+  """ A class for cleaning up your data!"""
 
+  def __init__(self, df):
+      self.df = df
+
+  def remove_nulls(self):
+      self.df
 
