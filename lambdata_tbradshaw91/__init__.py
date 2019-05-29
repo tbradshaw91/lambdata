1936267daa7205f966b50c34f21a014e09756e09
SC <SC@StrietzelCosMBP.home>
2019-05-28 17:23:55 -0700

    Commit

--- lambdata_tbradshaw91/__init__.py
+++ lambdata_tbradshaw91/__init__.py
@@ -9,3 +9,21 @@ from . import example_module
 Y = example_module.increment(example_module.x)
 TEST = pd.DataFrame(np.ones(10))
 
+""" For Fun """
+class Dog:
+  """ Testing Woof """
+  def __init__(self, name):
+     self.name = name
+     self.tricks = []
+
+  def add_trick(self, trick):
+      self.tricks.append(trick)
+
+class DataCleaner:
+  """A Class for cleaning up your data"""
+   def __init__(self, df):
+       self.df = df
+
+   def remove_nulls(self):
+       self.df
+
